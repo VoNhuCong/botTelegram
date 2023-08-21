b984f7cb6db68e202eabe851adf96bf0afa5aa30
Võ Như Công <82624211+VoNhuCong@users.noreply.github.com>
2023-08-21 15:05:02 +0700

    update docker volume

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # botTelegram
+
 Nhận tin nhắn của người nhắn sau đó gửi lại một phản hồi
 
 # to get Acces token
+
 find BotFather and chat
 /newbot
 enter your chat_bot_name
 
 # to runing
+
 cmd: nodemon ./src/index.js
 
 # to build docker
-Docker build -t bottelegram .
-# to run docker
-docker run -dp 127.0.0.1:3000:3000 bottelegram   
 
+Docker build -t bottelegram .
+
+# to run docker
+
+docker run -dp 127.0.0.1:3000:3000 bottelegram
+
+# to run with volume
+
+docker run -v myvolume:/usr/src/app/resources -p 3000:3000 telegrambot
+
+docker run -v D:/workspace:/usr/src/app/resources -p 3000:3000 telegrambot
--- src/TelegramBot.js
+++ src/TelegramBot.js
@@ -1,4 +1,8 @@
-console.log("telegram bot");
+const fs = require("fs");
+const filePath = "./resources/data.txt";
+
+console.log('Bot listening');
+
 // Require the node-telegram-bot-api module
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
@@ -9,49 +13,20 @@ const token = process.env.TOKEN;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("polling_error", console.log);
-
-bot.onText(/^\/start/, function (msg, match) {
-    var option = {
-        "parse_mode": "Markdown",
-        "reply_markup": {
-            "one_time_keyboard": true,
-            "keyboard": [[{
-                text: "My phone number",
-                request_contact: true
-            }], ["Cancel"]]
-        }
-    };
-    bot.sendMessage(msg.chat.id, "How can we contact you?", option).then(() => {
-        bot.once("contact", (msg) => {
-            var option = {
-                "parse_mode": "Markdown",
-                "reply_markup": {
-                    "one_time_keyboard": true,
-                    "keyboard": [[{
-                        text: "My location",
-                        request_location: true
-                    }], ["Cancel"]]
-                }
-            };
-        })
-    })
-
-});
-
 bot.on('message', (message) => {
     let chat_id = message.from.id;
-    if (message.contact) {
-        // import to db include: chat_id and phone_number
-        console.log("get contact: ", message.contact.phone_number);
-    }
-    console.log("get contact: ", message);
+    console.log("check chat_id: ", chat_id);
+    writeStringTofile("xin chao, toi la Vo Nhu Cong");
+    bot.sendMessage(chat_id, "xin chao, toi la Vo Nhu Cong");
 });
 
-function sendMessage(chat_id, message) {
-    bot.sendMessage(chat_id, message);
-}
-
-module.exports = {
-    sendMessage
+let writeStringTofile = function (text) {
+    fs.writeFile(filePath, text, (err) => {
+        if (err) {
+            console.log("File written pheeu");
+            console.error(err);
+        } else {
+            console.log("File written successfully.");
+        }
+    });
 }
